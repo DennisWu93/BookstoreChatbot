5ff82601d2c03fe2e75b5f5f3e8507f0616c2dce
DennisWu93 <rongzong.wu@alumni.anu.edu.au>
2025-08-06 14:01:31 +0800

    git commit -m "test pipeline in 0806"

--- chatbot.py
+++ chatbot.py
@@ -1,2 +1,2 @@
 def greet():
-    return "Welcome to ABC Bookstore! How can I help you find your next great read today?"
+    return "Welcome to ABC Bookstore! How can I help you find your next great read todaYy?"
